8fb7d3f5b07722732b01305b44751ba9f2714155
ebertolo <ebertolo@pop-os.localdomain>
2019-11-08 22:13:06 -0300

    app start configuration

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from flask import Flask
 app = Flask(__name__)
 
-
 @app.route('/')
 def index():
-    return '<h1>Hello World 5</h1>'
+    return '<h1>EVND/h1>'
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
