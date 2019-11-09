88309cc1a2e9d6353eb75ce85ba563bc5baf9df4
ebertolo <ebertolo@pop-os.localdomain>
2019-11-09 15:02:24 -0300

    templates base e paginas de erros

--- app.py
+++ app.py
@@ -1,9 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
-@app.route('/')
+# Rotinas Principais
+@app.route("/")
 def index():
-    return '<h1>EVND/h1>'
+    return render_template("pages/index.html", pagina="EVND - Home")
 
+@app.route("/item/<name>")
+def item(name):
+    return render_template("pages/home.html", name=name, items=["arroz", "feijao", "farinha" , "teste"])
+
+
+# Paginas de Erros customizados
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("exceptions/404.html"), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template("exceptions/500.html"), 500
+
+
+# Permite execucao via script
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
