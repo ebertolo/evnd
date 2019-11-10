490c7221ff3c0bcecd59346b23270a8c621626a6
ebertolo <ebertolo@pop-os.localdomain>
2019-11-10 14:03:16 -0300

    Deleta arquivos demos

--- app.py
+++ app.py
@@ -1,17 +1,39 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+from flask_moment import Moment
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'endgame infinite-war age-of-ultron the-avengers'
+
 bootstrap = Bootstrap(app)
+moment = Moment(app)
+
+
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
 
 # Rotinas Principais
 @app.route("/")
 def index():
-    return render_template("pages/index.html", pagina="EVND - Home")
+    return render_template("pages/index.html", page="Home")
 
-@app.route("/item/<name>")
-def item(name):
-    return render_template("pages/home.html", name=name, items=["arroz", "feijao", "farinha" , "teste"])
+@app.route("/product/<name>")
+def product(name):
+    return render_template("pages/home.html", page=name, items=["arroz", "feijao", "farinha" , "teste"])
+
+@app.route("/customer/<id>", methods=["GET", "POST"])
+def customer(id):
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template("pages/customer.html", form=form, page="Clientes", id=id)
+
 
 
 # Paginas de Erros customizados
