37716be36906c75b1161f648b15e972f980417f1
Emerson <ebertolo@outlook.com>
2020-09-10 01:53:57 -0300

    Utilizar lib Flask-Awesome

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ import os
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
+from flask_fontawesome import FontAwesome
 from flask import Flask
 
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -12,6 +13,8 @@ app.config["SECRET_KEY"] = "endgame infinite-war age-of-ultron the-avengers"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(basedir, "data.sqlite")
 app.config["SQLALCHEMY_COMMIT_ON_TEARDOWN"] = True
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
+
+fa = FontAwesome(app)
 
 db = SQLAlchemy(app)
 db.create_all()
