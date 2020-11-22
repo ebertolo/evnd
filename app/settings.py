ceb321c0ea83f28cc5e8cce802c202c1e8047f4f
Emerson <ebertolo@outlook.com>
2020-11-22 20:50:50 -0300

    Fim Projeto

--- app/settings.py
+++ app/settings.py
@@ -12,11 +12,3 @@ SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "data.sqlite")
 SQLALCHEMY_COMMIT_ON_TEARDOWN = True 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-# Parametros do Email
-MAIL_HOSTNAME = "smtp.gmail.com"
-MAIL_SERVER = "smtp.gmail.com"
-MAIL_PORTER = 465
-MAIL_USE_TLS = False
-MAIL_USE_SSL = True
-MAIL_USERNAME = "evnd.unisul@gmail.com" #Email criado apenas para o projeto por isso foi mantido a senha no codigo
-MAIL_PASSWORD = "%TGB6yhn"
